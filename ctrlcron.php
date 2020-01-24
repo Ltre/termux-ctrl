2c63065fb3f108961383f78cc05c297c4c7df874
Ltre <ltrele@yeah.net>
2020-01-24 14:27:21 +0800

    fdasf

--- ctrlcron.php
+++ ctrlcron.php
@@ -17,14 +17,14 @@ $hour = 14;
 for ($i = 0; $i < 1000; $i ++) {
 	if ($hour >= 9 && $hour <= 21) {
         if ($now%3600 <= 10) {//每小时播放普通音量的声音
-        	// shell_exec('mpv putong.mp3');
+        	shell_exec('mpv putong.mp3');
 			print 1;
     	} else {
 			print 'A';
 		}
 	} else {
     	if ($now%3600 <= 10) {//每小时播放声音不算大的声音
-        	// shell_exec('mpv feng.mp3');
+        	shell_exec('mpv feng.mp3');
 			print 2;
     	} else {
 			print 'B';
